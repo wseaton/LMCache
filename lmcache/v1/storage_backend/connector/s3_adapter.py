3881a77f3e2170c299a6c762726029a020cb9b20
Will Eaton <weaton@redhat.com>
2025-11-05 16:59:08 -0500

    cleanup
    Signed-off-by: Will Eaton <weaton@redhat.com>

--- lmcache/v1/storage_backend/connector/s3_adapter.py
+++ lmcache/v1/storage_backend/connector/s3_adapter.py
@@ -13,7 +13,6 @@ from lmcache.v1.storage_backend.connector.base_connector import RemoteConnector
 logger = init_logger(__name__)
 
 # environment variables to control which implementation to use
-USE_CRT_S3 = os.getenv("LMCACHE_USE_CRT_S3", "0") == "1"
 USE_RUST_S3 = os.getenv("LMCACHE_USE_RUST_S3", "0") == "1"
 
 
@@ -48,25 +47,8 @@ class S3ConnectorAdapter(ConnectorAdapter):
         s3_endpoint = context.url
 
         # choose implementation based on environment variables
-        # priority: CRT > Rust > Python
-        if USE_CRT_S3:
-            logger.info("Using C++ AWS CRT S3 connector implementation")
-            # Local
-            from .crt_s3_connector import CrtS3Connector
-
-            return CrtS3Connector(
-                s3_endpoint=s3_endpoint,
-                loop=context.loop,
-                local_cpu_backend=context.local_cpu_backend,
-                s3_part_size=self.s3_part_size,
-                s3_file_prefix=self.s3_file_prefix,
-                s3_max_io_concurrency=self.s3_max_io_concurrency,
-                s3_max_inflight_reqs=self.s3_max_inflight_reqs,
-                s3_prefer_http2=self.s3_prefer_http2,
-                s3_region=self.s3_region,
-                s3_enable_s3express=self.s3_enable_s3express,
-            )
-        elif USE_RUST_S3:
+        # priority: Rust > Python
+        if USE_RUST_S3:
             logger.info("Using Rust S3 connector implementation")
             # Local
             from .rust_s3_connector import RustS3Connector
